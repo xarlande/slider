7b0cfe6033531a5c39b7e054e4554c2cff2a35ec
bogdan <0966312126a@gmail.com>
2023-06-08 16:54:01 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { selectorsQuery } from "./modules/consol.js";
+
+const selectors = selectorsQuery();
+
+selectors.btn.addEventListener("click", () => {});
---
+++ js/modules/consol.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".btn");
+
+export function selectorsQuery() {
+  return { btn };
+}
