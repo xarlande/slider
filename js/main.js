ddff6992b828414747413ca087d7630329df23eb
bogdan <0966312126a@gmail.com>
2023-06-09 12:41:31 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,14 +2,68 @@ import { selectorsQuery } from "./modules/consol.js";
 
 const selectors = selectorsQuery();
 
-selectors.nextBtn.addEventListener("click", () => console.log("Next"));
+let currentImg = 0;
+
+const allImgLength = selectors.allImg.length;
+const maxCurrentImg = allImgLength - 1;
+
+const active = " ● ";
+const notActive = " ○ ";
+
+const generateBtn = () => {
+  const itemsArr = [];
+  let itemsString = "";
+
+  for (let i = 0; i < allImgLength; i += 1) {
+    itemsArr.push(notActive);
+  }
+
+  itemsArr.splice(currentImg, 1, active);
+
+  let index = 0;
+
+  itemsArr.forEach((item) => {
+    itemsString =
+      itemsString + `<span index='${index}'> ${item} </span>` + "\n";
+    index += 1;
+  });
+
+  return itemsString;
+};
+
+const updateHtmlForButtons = () =>
+  (selectors.btnBottomBlock.innerHTML = generateBtn());
+
+updateHtmlForButtons();
+
+selectors.btnBottomBlock.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.closest(".btn__block-field span")) {
+    currentImg = target.attributes.index.value;
+    updateHtmlForButtons();
+  }
+});
+
+selectors.nextBtn.addEventListener("click", () => {
+  if (maxCurrentImg < currentImg + 1) {
+    currentImg = 0;
+  } else {
+    currentImg += 1;
+  }
+  updateHtmlForButtons();
+});
+selectors.backBtn.addEventListener("click", () => {
+  if (currentImg > 0) {
+    currentImg -= 1;
+  } else {
+    currentImg = maxCurrentImg;
+  }
+  updateHtmlForButtons();
+});
 
 class Slider {
-
-
-    constructor() {
-
-    }
+  constructor() {}
 }
 
-const Sli = new Slider()
+const Sli = new Slider();
--- js/modules/consol.js
+++ js/modules/consol.js
@@ -1,6 +1,9 @@
 const nextBtn = document.querySelector("#next-btn");
 const backBtn = document.querySelector("#back-btn");
+const allImg = document.querySelectorAll(".img__block img")
+const btnBottomBlock = document.querySelector('.btn__block-field')
+const btnBottomAll = document.querySelector('.btn__block-field span')
 
 export function selectorsQuery() {
-  return { nextBtn, backBtn };
+  return { nextBtn, backBtn, allImg, btnBottomBlock, btnBottomAll };
 }
