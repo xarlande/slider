e1a7d6c947315fad47c82a31aadb3ee79866c3a1
bogdan <0966312126a@gmail.com>
2023-06-10 19:17:24 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,117 +2,140 @@ import { selectorsQuery } from "./modules/consol.js";
 
 const selectors = selectorsQuery();
 
-let currentImgIndex = 0;
-let currentShowType = 1;
-
-const allImgLength = () => selectors.allImg.length;
-const maxImgIndex = allImgLength() - 1;
-
-const active = " ● ";
-const notActive = " ○ ";
-
-const generateBtn = () => {
-  const itemsArr = [];
-  let itemsString = "";
-
-  for (let i = 0; i < allImgLength(); i += 1) {
-    itemsArr.push(notActive);
+class Carousel {
+  constructor() {
+    this.currentImgIndex = 0;
+    this.currentShowType = 1;
+    this.active = " ● ";
+    this.notActive = " ○ ";
   }
 
-  itemsArr.splice(currentImgIndex, 1, active);
-
-  let index = 0;
-
-  itemsArr.forEach((item) => {
-    itemsString =
-      itemsString + `<span index='${index}'> ${item} </span>` + "\n";
-    index += 1;
-  });
-
-  return itemsString;
-};
-
-const updateHtmlForButtons = () =>
-  (selectors.btnBottomBlock.innerHTML = generateBtn());
-const updateImgBlockStyle = () => {
-  if (currentShowType === 1) {
-    selectors.imgBlock.style.transform = `translate3d(${
-      -1300 * currentImgIndex
-    }px, 0px, 0px)`;
+  getAllImgLength() {
+    return selectors.allImg.length;
   }
-  if (currentShowType === 3) {
-    selectors.imgBlock.style.transform = `translate3d(${
-      -(1330 / 3) * currentImgIndex
-    }px, 0px, 0px)`;
+
+  getMaxImgIndex() {
+    return this.getAllImgLength() - 1;
   }
-};
 
-const updateDOM = () => {
-  updateHtmlForButtons();
-  updateImgBlockStyle();
-};
+  generateBtn() {
+    const itemsArr = [];
+    let itemsString = "";
 
-selectors.btnBottomBlock.addEventListener("click", (event) => {
-  const target = event.target;
+    for (let i = 0; i < this.getAllImgLength(); i += 1) {
+      itemsArr.push(this.notActive);
+    }
 
-  if (target.closest(".btn__block-field span")) {
-    currentImgIndex = +target.attributes.index.value;
-    updateDOM();
+    itemsArr.splice(this.currentImgIndex, 1, this.active);
+
+    let index = 0;
+
+    itemsArr.forEach((item) => {
+      itemsString =
+          itemsString + `<span index='${index}'> ${item} </span>` + "\n";
+      index += 1;
+    });
+
+    return itemsString;
   }
-});
 
-selectors.nextBtn.addEventListener("click", () => {
-  if (currentShowType === 3) {
-    if (maxImgIndex < currentImgIndex + 3) {
-      currentImgIndex = 0;
-    } else {
-      currentImgIndex += 3;
+  updateHtmlForButtons() {
+    selectors.btnBottomBlock.innerHTML = this.generateBtn();
+  }
+
+  updateImgBlockStyle() {
+    if (this.currentShowType === 1) {
+      selectors.imgBlock.style.transform = `translate3d(${
+          -1300 * this.currentImgIndex
+      }px, 0px, 0px)`;
+    }
+    if (this.currentShowType === 3) {
+      selectors.imgBlock.style.transform = `translate3d(${
+          -(1330 / 3) * this.currentImgIndex
+      }px, 0px, 0px)`;
     }
   }
-  if (currentShowType === 1) {
-    if (maxImgIndex < currentImgIndex + 1) {
-      currentImgIndex = 0;
-    } else {
-      currentImgIndex += 1;
-    }
-  }
-  updateDOM();
-});
-selectors.backBtn.addEventListener("click", () => {
-  if (currentShowType === 3) {
-    if (currentImgIndex > 2) {
-      currentImgIndex -= 3;
-    } else if (currentImgIndex > 0) {
-      currentImgIndex = 0;
-    } else {
-      currentImgIndex = maxImgIndex - 2;
-    }
-  }
-  if (currentShowType === 1) {
-    if (currentImgIndex > 0) {
-      currentImgIndex -= 1;
-    } else {
-      currentImgIndex = maxImgIndex;
-    }
-  }
-  updateDOM();
-});
-selectors.typeShowSelect.addEventListener("change", (event) => {
-  currentShowType = +event.target.value;
-  currentImgIndex = 0;
-  updateStyleForImage();
-  updateDOM();
-});
 
-const updateStyleForImage = () => {
-  selectors.allImg.forEach((item) => {
-    item.style.width = `${100 / currentShowType}%`;
-    item.style.borderRadius = "4px";
-    item.style.height = "100%";
-  });
-};
-updateDOM();
-updateStyleForImage();
+  updateDOM() {
+    this.updateHtmlForButtons();
+    this.updateImgBlockStyle();
+  }
+
+  handleButtonClick(event) {
+    const target = event.target;
+
+    if (target.closest(".btn__block-field span")) {
+      this.currentImgIndex = +target.attributes.index.value;
+      this.updateDOM();
+    }
+  }
+
+  handleNextButtonClick() {
+    if (this.currentShowType === 3) {
+      if (this.getMaxImgIndex() < this.currentImgIndex + 3) {
+        this.currentImgIndex = 0;
+      } else {
+        this.currentImgIndex += 3;
+      }
+    }
+    if (this.currentShowType === 1) {
+      if (this.getMaxImgIndex() < this.currentImgIndex + 1) {
+        this.currentImgIndex = 0;
+      } else {
+        this.currentImgIndex += 1;
+      }
+    }
+    this.updateDOM();
+  }
+
+  handleBackButtonClick() {
+    if (this.currentShowType === 3) {
+      if (this.currentImgIndex > 2) {
+        this.currentImgIndex -= 3;
+      } else if (this.currentImgIndex > 0) {
+        this.currentImgIndex = 0;
+      } else {
+        this.currentImgIndex = this.getMaxImgIndex() - 2;
+      }
+    }
+    if (this.currentShowType === 1) {
+      if (this.currentImgIndex > 0) {
+        this.currentImgIndex -= 1;
+      } else {
+        this.currentImgIndex = this.getMaxImgIndex();
+      }
+    }
+    this.updateDOM();
+  }
+
+  handleTypeShowSelectChange(event) {
+    this.currentShowType = +event.target.value;
+    this.currentImgIndex = 0;
+    this.updateStyleForImage();
+    this.updateDOM();
+  }
+
+  updateStyleForImage() {
+    selectors.allImg.forEach((item) => {
+      item.style.width = `${100 / this.currentShowType}%`;
+      item.style.borderRadius = "4px";
+      item.style.height = "100%";
+    });
+  }
+
+  init() {
+    selectors.btnBottomBlock.addEventListener("click", this.handleButtonClick.bind(this));
+    selectors.nextBtn.addEventListener("click", this.handleNextButtonClick.bind(this));
+    selectors.backBtn.addEventListener("click", this.handleBackButtonClick.bind(this));
+    selectors.typeShowSelect.addEventListener("change", this.handleTypeShowSelectChange.bind(this));
+    this.updateDOM();
+    this.updateStyleForImage();
+  }
+}
+
+const NewCarousel = new Carousel();
+NewCarousel.init();
+
 
 // class Father {}
 //
